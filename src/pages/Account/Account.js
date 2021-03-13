30b55e51a8ae84970013e7827ca70e54b62fd245
Matías Báez <matiasbaez2512@gmail.com>
2021-03-13 19:03:52 -0300

    Updated, components and added guest page

---
+++ src/pages/Account/Account.js
@@ -0,0 +1,22 @@
+
+import React, { useEffect, useState } from 'react';
+import * as firebase from 'firebase';
+
+import UserGuest from './UserGuest';
+import UserLogged from './UserLogged';
+import Loading from '../../components/Loading';
+
+export default function Account() {
+
+    const [login, setLogin] = useState(null);
+
+    useEffect(() => {
+        firebase.auth().onAuthStateChanged(user => {
+            user ? setLogin(true) : setLogin(false);
+        });
+    }, [])
+
+    if (login === null) return <Loading isVisible={login} text="Cargando..." />;
+
+    return login ? <UserLogged /> : <UserGuest />;
+}
